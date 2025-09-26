a529387839c12f7c69abfe7cdc54ce0669c5d31f
Penko Atanasov <atanasovpenko@gmail.com>
2025-09-26 18:58:20 +0300

    Removing unused props

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,2 +1,2 @@
 export { Button, type ButtonProps, type ButtonSize, type ButtonVariant } from "./Button";
-export { Input, type InputProps, type InputSize, type InputVariant } from "./Input";
+export { Input, type InputProps } from "./Input";

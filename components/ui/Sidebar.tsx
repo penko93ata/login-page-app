e64aab9eaf1311784004ad376e7fe79f949fb3c4
Penko Atanasov <atanasovpenko@gmail.com>
2025-09-27 10:38:07 +0300

    Setting language correctly

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -3,21 +3,35 @@
 import { useState } from "react";
 
 import styles from "./Sidebar.module.css";
+import { useLocale } from "next-intl";
+import { usePathname, useRouter } from "@/i18n/navigation";
+import { routing } from "@/i18n/routing";
+
+type LanguageItem = {
+  code: (typeof routing.locales)[number];
+  flag: string;
+  name: string;
+};
+
+const navigationItems = [
+  { key: "about", label: "About" },
+  { key: "contact", label: "Contact" },
+];
+
+const languages: LanguageItem[] = [
+  { code: "en", flag: "🇺🇸", name: "English" },
+  { code: "bg", flag: "🇧🇬", name: "Български" },
+];
 
 export default function Sidebar() {
   const [isLanguageDropdownOpen, setIsLanguageDropdownOpen] = useState(false);
+  const locale = useLocale();
+  const router = useRouter();
+  const pathname = usePathname();
+  console.log({ locale, pathname });
 
-  const navigationItems = [
-    { key: "about", label: "About" },
-    { key: "contact", label: "Contact" },
-  ];
-
-  const languages = [
-    { code: "en", flag: "🇺🇸", name: "English" },
-    { code: "bg", flag: "🇧🇬", name: "Български" },
-  ];
-
-  //   const currentLanguage = languages.find((lang) => lang.code === language)
+  const currentLanguage = languages.find((lang) => lang.code === locale);
+  console.log({ currentLanguage });
 
   return (
     <aside className={styles.sidebar}>
@@ -39,8 +53,8 @@ export default function Sidebar() {
 
       <div className={styles.languageSelector}>
         <button className={styles.languageButton} onClick={() => setIsLanguageDropdownOpen(!isLanguageDropdownOpen)}>
-          <span className={styles.languageFlag}>🇺🇸</span>
-          <span className={styles.languageName}>English</span>
+          <span className={styles.languageFlag}>{currentLanguage?.flag}</span>
+          <span className={styles.languageName}>{currentLanguage?.name}</span>
           <span className={`${styles.dropdownArrow} ${isLanguageDropdownOpen ? styles.open : ""}`}>▼</span>
         </button>
 
@@ -49,8 +63,10 @@ export default function Sidebar() {
             {languages.map((lang) => (
               <button
                 key={lang.code}
-                className={`${styles.languageOption} ${"en" === lang.code ? styles.selected : ""}`}
+                className={`${styles.languageOption} ${locale === lang.code ? styles.selected : ""}`}
                 onClick={() => {
+                  router.replace(pathname, { locale: lang.code });
+                  router.refresh();
                   setIsLanguageDropdownOpen(false);
                 }}
               >
